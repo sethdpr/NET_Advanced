53488f8ba0364ef02d39f916dc308d47a1b8bb23
Seth De Preter <seth.de.preter@student.ehb.be>
2025-02-26 16:16:29 +0100

    Working userlist

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using NET_Advanced.Areas.Identity.Data;
+using NET_Advanced.Data;
 
 namespace NET_Advanced.Controllers
 {
@@ -17,23 +18,26 @@ namespace NET_Advanced.Controllers
     {
         private readonly UserManager<NET_AdvancedUser> _userManager;
 
+        // Constructor die de UserManager injecteert voor toegang tot Identity
         public APIController(UserManager<NET_AdvancedUser> userManager)
         {
             _userManager = userManager;
         }
 
-        // GET: api/users
-        [HttpGet("/users")]
+        // GET: api/users (Haalt alle gebruikers op)
+        [HttpGet("users")]
         public async Task<ActionResult<IEnumerable<NET_AdvancedUser>>> GetAllUsers()
         {
+            // Haal alle gebruikers op uit de IdentityDbContext
             var users = await _userManager.Users.ToListAsync();
 
+            // Als er geen gebruikers zijn
             if (users == null || users.Count == 0)
             {
                 return NotFound("Geen gebruikers gevonden.");
             }
 
-            return Ok(users);
+            return Ok(users);  // Retourneer de lijst van gebruikers
         }
     }
 }
--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,30 +4,35 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using NET_Advanced.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace NET_Advanced.Controllers
 {
     public class AdminController : Controller
     {
         private readonly HttpClient _httpClient;
+        private readonly UserManager<NET_AdvancedUser> _userManager;
 
-        public AdminController(HttpClient httpClient)
+        public AdminController(HttpClient httpClient, UserManager<NET_AdvancedUser> userManager)
         {
             _httpClient = httpClient;
+            _userManager = userManager;
         }
 
         // GET: /Admin/Index
         public async Task<IActionResult> Index()
         {
-            var response = await _httpClient.GetAsync("https://localhost:7156/api/API/users");
-            if (response.IsSuccessStatusCode)
+            var users = await _userManager.Users.ToListAsync();  // Haalt de gebruikers op uit de Identity DB.
+
+            // Als er geen gebruikers zijn, geef een lege lijst door aan de view
+            if (users == null || users.Count == 0)
             {
-                var usersJson = await response.Content.ReadAsStringAsync();
-                var users = JsonConvert.DeserializeObject<List<NET_AdvancedUser>>(usersJson);
-                return View(users);
+                return View(new List<NET_AdvancedUser>());
             }
 
-            return View(new List<NET_AdvancedUser>());
+            // Geef de gebruikerslijst door aan de view
+            return View(users);
         }
     }
 }
